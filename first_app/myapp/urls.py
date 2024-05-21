9b97b05f57d5bb3bd8178df3ba0efe4111895827
ramanath-naik <108948463+ramanath-naik@users.noreply.github.com>
2024-05-21 18:46:38 +0530

    caching in django using redis

--- first_app/myapp/urls.py
+++ first_app/myapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('',views.home, name='home')
+    #   path('',views.test_redis, name='test_redis')
 ]
--- first_app/myapp/views.py
+++ first_app/myapp/views.py
@@ -1,7 +1,48 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse
+from django.core.cache import cache
+
+from .models import *
 
 # Create your views here.
 
+# def home(request):
+#     objs = Fruits.objects.all()
+
+#     payload = []
+#     for obj in objs:
+#         payload.append(obj.fruit_name)
+
+#     return JsonResponse({'status' : 200, 'db': 'sqlite', 'data': payload});
+
+
 def home(request):
-    return HttpResponse("Hello World");
+    payload = []
+    db = None
+
+    if cache.get('fruits'):
+        payload = cache.get('fruits')
+        db = 'redis'
+        print(cache.ttl("fruits"))
+    else:
+        objs = Fruits.objects.all()
+        for obj in objs:
+            payload.append(obj.fruit_name)
+        db = 'sqlite'
+
+        cache.set('fruits',payload, timeout=10)
+
+    return JsonResponse({'status' : 200, 'db': db, 'data': payload});
+
+# def test_redis(request):
+#     try:
+#         # Attempt to set and get a value in the Redis cache
+#         cache.set('test_key', 'test_value', timeout=30)
+#         value = cache.get('test_key')
+#         if value == 'test_value':
+#             return JsonResponse({'status': 'success', 'message': 'Redis is working!'})
+#         else:
+#             return JsonResponse({'status': 'error', 'message': 'Redis is not returning the correct value.'})
+#     except Exception as e:
+#         return JsonResponse({'status': 'error', 'message': str(e)})
