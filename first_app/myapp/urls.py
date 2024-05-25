6e755a9d47ef4f55e8f90c0d4ef04aa6b8ec5953
ramanath-naik <108948463+ramanath-naik@users.noreply.github.com>
2024-05-25 09:26:44 +0530

    adding two numbers

--- first_app/myapp/urls.py
+++ first_app/myapp/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('',views.home, name='home'),
-    path('hello', views.hello, name='hello')
+    path('hello', views.hello, name='hello'),
+    path('add', views.add, name='add')
     #   path('',views.test_redis, name='test_redis')
 ]
