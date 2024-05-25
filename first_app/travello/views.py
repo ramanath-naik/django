2fe0bda5e66a69336b92e43930033eb1fb869a7b
ramanath-naik <108948463+ramanath-naik@users.noreply.github.com>
2024-05-25 12:25:55 +0530

    passing image as dynamic and also passing multiple data using for loop

--- first_app/travello/views.py
+++ first_app/travello/views.py
@@ -4,7 +4,15 @@ from . models import Destination
 # Create your views here.
 
 def index(request):
+
     dest1 = Destination()
     dest1.name = 'mumbai'
-    # dest1.img = '2.png'
-    return render(request, 'index.html', {'dest1': dest1}) 
+    dest1.img = '2.png'
+
+    dest2 = Destination()
+    dest2.name = 'Bengaluru'
+    dest2.img = '5.png'
+
+    dests = [dest1, dest2]
+
+    return render(request, 'index.html', {'dests': dests}) 
