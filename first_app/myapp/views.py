900319e48a2547e77b716a420ab4680328861669
ramanath-naik <108948463+ramanath-naik@users.noreply.github.com>
2024-05-25 09:46:14 +0530

    using post method

--- first_app/myapp/views.py
+++ first_app/myapp/views.py
@@ -52,7 +52,7 @@ def hello(request):
     return render(request, 'home.html',{'name':'naveen'})  #to pass dynamic value
 
 def add(request):
-    val1= int(request.GET['num1'])
-    val2= int(request.GET['num2'])
+    val1= int(request.POST['num1'])  #we are not sending data in url. (i.e importance of post method)
+    val2= int(request.POST['num2'])
     res = val1+val2
     return render(request, "result.html", {'result':res})
